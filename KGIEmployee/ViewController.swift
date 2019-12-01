8845a6853d272c3e909d5e9e8b4014cfac4e0363
pachie <archieangeles@hotmail.com>
2019-12-02 01:04:10 +0800

    Initial Commit

---
+++ KGIEmployee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KGIEmployee
+//
+//  Created by Sembawang Housemate on 2/12/19.
+//  Copyright © 2019 boopbayssoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
